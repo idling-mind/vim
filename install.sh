446f27a12edb0bb87a2d8786952ee7bc7d615bd2
Najeem Muhammed <najeem.muhammed@gknaerospace.com>
2019-05-03 11:55:56 +0530

    directory create bug

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Script to set up vim
 curpath=`pwd`
-ln -sv $curpath/.vim $HOME/
-ln -sv $curpath/.vimrc $HOME/
 
-mkdir ~/.vim/bundle
+mkdir -p .vim/bundle
+
+ln -sv $curpath/.vim $HOME/.vim
+ln -sv $curpath/.vimrc $HOME/.vimrc
+
 cd ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git
 
+echo "-------------------------------------------------"
 echo "Now open vim and enter the command :PluginInstall"
